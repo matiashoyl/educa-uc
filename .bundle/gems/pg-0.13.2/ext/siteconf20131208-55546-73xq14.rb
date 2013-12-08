e2d440acc70e693df22e35233a20eace57b9ddcb
Matias Hoyl <matiashoyl@gmail.com>
2013-12-08 18:45:51 -0300

    Deploy a heroku

---
+++ .bundle/gems/pg-0.13.2/ext/siteconf20131208-55546-73xq14.rb
@@ -0,0 +1,6 @@
+require 'rbconfig'
+dest_path = "./.gem.20131208-55546-1bwqjdi"
+RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
+RbConfig::CONFIG['sitearchdir'] = dest_path
+RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
+RbConfig::CONFIG['sitelibdir'] = dest_path
